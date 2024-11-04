cdabc9230af01854869e4a67f5ead1b89934a743
Kevin Wu <28658818+ypwk@users.noreply.github.com>
2024-11-03 18:07:19 -0800

    Get rid of amplify apps

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./assets/main.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-
-Amplify.configure(outputs);
-
-createApp(App).mount("#app");
